691dd0ed3ebc3d75ced35cde7ab5ae416b66680b
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-02-21 00:36:36 -0500

    started implementing footer

---
+++ sammyrp/src/components/common/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="footer-wrapper" id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <p class="section-p cp">
+            {{ this.Footer.COPYRIGHT }}
+            <a v-bind:href="`mailto:${this.Footer.Links.LINK}`">{{ this.Footer.Links.LINK }}</a>
+          </p>
+          <p class="section-p cp">{{ this.LastUpdatedDate }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// import DataHandlers from '../utils/getters'
+import DATA from '../../constants'
+export default {
+  name: 'Footer',
+  created() {
+    const { Footer } = DATA
+    this.Footer = Footer
+    this.LastUpdatedDate = '' //`Last updated: ${DataHandlers.getDataFromGithub('github')}`
+  },
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+.footer-wrapper {
+  font-family: 'Montserrat', sans-serif !important;
+}
+.cp {
+  margin-top: 2rem;
+}
+.section-p {
+  text-align: center;
+  mix-blend-mode: difference !important;
+  -webkit-mix-blend-mode: difference;
+  -moz-mix-blend-mode: difference;
+  color: #afafaf;
+}
+</style>
--- sammyrp/src/components/page-components/Home.vue
+++ sammyrp/src/components/page-components/Home.vue
@@ -7,18 +7,21 @@
       v-bind:subSubText="this.Strings.ANNOUNCEMENT"
       v-bind:hasAnimation="true"
     ></PageTile>
+    <Footer></Footer>
   </div>
 </template>
 <script>
 import Navbar from '../common/Navbar'
 import PageTile from '../common/pageTitle'
 import DATA from '../../constants'
+import Footer from '../common/Footer'
 export default {
   name: 'Home',
   props: {},
   components: {
     Navbar,
     PageTile,
+    Footer,
   },
   created() {
     const { Strings } = DATA.Home
