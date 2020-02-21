8c0234abd1862841d4c30c415545853a761bbf73
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-02-21 11:48:02 -0500

    changed to use axios

--- sammyrp/src/components/utils/getters.js
+++ sammyrp/src/components/utils/getters.js
@@ -1,42 +1,12 @@
 /**@format */
-
+import axios from 'axios'
 import DATA from '../../constants'
-const ERR = "403 Error: Unable to Reach Github's server"
-const GH = 'github'
+
+const GH_URL = `https://api.github.com/repos/${DATA.USERNAME}/${DATA.REPO_NAME}/commits/master`
 
 const DataHandlers = {
-  /**
-   * gets last commit date from github
-   * @param {String} api
-   * @returns Date
-   * @readonly
-   */
-  getDataFromGithub(api) {
-    if (api === GH) {
-      try {
-        const xmlhttp = new XMLHttpRequest()
-        xmlhttp.onreadystatechange = () => {
-          if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            const gitRef = JSON.parse(xmlhttp.responseText)
-            let { date } = gitRef.commit.author
-            date = date.substring(0, 10)
-            return date
-          } else {
-            return ERR
-          }
-        }
-        xmlhttp.open(
-          'GET',
-          `https://api.github.com/repos/${DATA.USERNAME}/${DATA.REPO_NAME}/commits/master`,
-          true,
-        )
-        xmlhttp.send()
-      } catch (error) {
-        return error
-      }
-    } else {
-      return ERR
-    }
+  getGithubHistory: async () => {
+    return await axios.get(GH_URL)
   },
 }
 export default { DataHandlers }
